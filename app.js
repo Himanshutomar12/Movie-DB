9157b820f14d55ca3fe49d941f2db88a008649c6
Himanshutomar12 <tomarhimanshu52@gmail.com>
2021-12-01 22:51:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const request = require("request");
+const https = require("https");
+const axios = require("axios");
+
+const app = express();
+//ejs templating
+app.set('view engine', 'ejs')
+//use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+//static files
+app.use(express.static("public"))
+
+
+
+
+app.get("/", function(req, res){
+     res.render("search")
+
+   });
+
+app.post("/", function(req, res){
+  const movieName = req.body.movieName;
+  const url = "https://www.omdbapi.com/?t=" + movieName + "&apikey=e4a05ad0";
+
+  axios.get(url).then(function(response){
+    console.log(response.data.Response);
+     if(response.data.Response === "True"){
+       res.render("success", {
+         title: response.data.Title,
+         released: response.data.Released,
+         genre: response.data.Genre,
+         plot: response.data.Plot,
+         language: response.data.Language,
+         poster: response.data.Poster,
+         director: response.data.Director,
+         award: response.data.Award,
+         imdb: typeof response.data.Ratings[0] === "undefined"? "-/-" :response.data.Ratings[0].Value,
+         rottenTomatoes: typeof response.data.Ratings[1] === "undefined"? "-/-" :response.data.Ratings[1].Value,
+         metacritic:  typeof response.data.Ratings[2] === "undefined"? "-/-" :response.data.Ratings[2].Value
+
+       })
+     }else{
+       res.render("failure");
+     }
+  })
+
+
+})
+
+app.post("/back", function(req, res){
+  res.redirect("/");
+})
+
+app.post("/searchFail", function(req, res){
+  res.redirect("/");
+})
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
